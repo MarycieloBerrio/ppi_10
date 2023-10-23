f4b625bbc1435e0c6f23a714bff09e5629b85f87
JeronimoVasquez <jevasquez@unal.edu.co>
2023-10-23 10:20:05 -0500

    Se crea la plantilla para el perfil de usuario

---
+++ pages/Perfil.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Perfil de Usuario")
+
+# Botón para mostrar el perfil del usuario
+if st.button("Mostrar Perfil"):
+    st.subheader("Perfil del Usuario")
+    st.write("Nombre: Juan")
+    st.write("Apellido: Pérez")
+    st.write("Género: Masculino")
+    st.write("Género Favorito: Aventuras")
+    st.write(f"Correo Electrónico: Jevasquez@unal.edu.co")
+    st.subheader("Juegos Calificados")
+    st.write("5")  
+    st.subheader("Juegos Finalizados")
+    st.write("10")  
