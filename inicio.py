b2c158b7721992929bdcb083e9e8ebec9584c5c9
JeronimoVasquez <jevasquez@unal.edu.co>
2023-10-30 09:49:09 -0500

    Se corrigen errores de perfil y logeo
    Se arregla el bug que reiniciaba la variable de logeo al navegar entre páginas, se agrega el requisito de estar logeado para calificar juegos y ver el perfil y se muestra la información correcta del usuario en su perfil

---
+++ inicio.py
@@ -0,0 +1,130 @@
+"""
+Este módulo genera toda la pantalla de inicio, además
+de ser la página que streamlit lee por defecto para 
+mostrar al usuario la aplicación
+"""
+# Importar las librerías requeridas
+import json
+import requests
+import streamlit as st
+
+# Configura el título y el favicon de la página
+st.set_page_config(
+    page_title="Gamer's Companion",
+    page_icon="🎮",
+    initial_sidebar_state="collapsed"
+)
+
+def local_css(file_name):
+    """
+    Carga un documento css y lo renderiza en la página.
+
+    Parameters:
+    file_name: Nombre del archivo a renderizar
+
+    Returns:
+    None.
+    """
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+# Llama a local_css con el nombre del archivo CSS
+local_css("style.css")
+
+# URL de la imagen del encabezado
+url_imagen = "https://i.imgur.com/qQH31fg.png?1"
+
+# Añade la imagen como encabezado de la aplicación de Streamlit
+st.image(url_imagen)
+
+# URL de la API de IGDB
+url = "https://api.igdb.com/v4/games"
+
+# Credenciales para la API de IGDB
+headers = {
+    'Client-ID': 'ju1vfy05jqstzoclqv1cs2hsomw1au',
+    'Authorization': 'Bearer 8h1ymcezojqdpcvmz5fvwxal2myoxp',
+}
+
+# Parámetros de la consulta a la API de IGDB
+body = 'fields name,cover.url; limit 100; sort rating desc;\
+        where rating > 70; where rating_count > 1000;'
+
+response = requests.post(url, headers=headers, data=body)
+
+# Comprueba si la solicitud fue exitosa
+if response.status_code == 200:
+    # Convierte la respuesta en JSON
+    games = json.loads(response.text)
+
+    # Contador para llevar un registro de cuántos juegos se han mostrado
+    count = 0
+
+    # Inicializa la fila HTML con el estilo de borde
+    row_html = "<table style='border-color: #fff;'><tr>"
+
+    # Muestra los juegos en Streamlit
+    for game in games:
+        if 'cover' in game and count < 50:
+            image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
+            image_url = 'https:' + image_url
+                    
+            # Incrementa el contador
+            count += 1
+    
+            # Añade el juego a la fila HTML
+            row_html += f"<td style='border-top: 1px solid #e7e7e7; border-bottom: 1px solid #e7e7e7; \
+                        border-left: 1px solid #0e1117; border-right: 1px solid #0e1117; width: \
+                        100px; height: 200px; text-align: center; vertical-align: top;'><img src='\
+                        {image_url}'style='width: 100px; object-fit: contain;'/><br/><div style=\
+                        'width: 100px; word-wrap: break-word;color: #e7e7e7;'>{game['name']}</div></td>"
+    
+            # Si se han añadido cinco juegos a la fila comienza una nueva
+            if count % 5 == 0:
+                row_html += "</tr></table>"
+                st.write(row_html, unsafe_allow_html=True)
+                row_html = "<table><tr>"
+                
+# Información de los desarrolladores
+developers = [
+    {"name": "Juan Gabriel Goez Duque ", "email": "jgoezd@unal.edu.co"},
+    {"name": "Jerónimo Vásquez Gónzalez ", "email": "jevasquez@unal.edu.co"},
+    {"name": "Marycielo Berrio Zapata ", "email": "mberrioz@unal.edu.co"},
+]
+
+# Crea el HTML para el pie de página
+footer_html = """
+<footer style='width: 100%; background-color: #333; padding: 20px 0; color: #fff;'>
+    <div style='max-width: 600px; margin: auto; text-align: left;'>
+        <h2 style='margin-bottom: 20px; color: #fff;'>Informacion de contacto</h2>
+"""
+
+for dev in developers:
+    footer_html += f"<p style='margin-bottom: 10px;'><strong style='color: #fff;'>\
+                {dev['name']}</strong>:<a href='mailto:{dev['email']}' style='color:\
+                #fff;'>{dev['email']}</a></p>"
+
+footer_html += """
+    </div>
+</footer>
+"""
+
+# Agrega un espacio en blanco al final de la página antes del pie de página
+st.write("<br/><br/><br/><br/>", unsafe_allow_html=True)
+
+# Muestra el pie de página en Streamlit
+st.markdown(footer_html, unsafe_allow_html=True)
+
+# Se crea un contador para la sesión de estado
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+# Se crea la variable logged_in si es la primera vez
+# que ingresa
+if st.session_state.count == 0:
+    st.session_state.logged_in = False
+
+# Se actualiza el contador para mantener el valor
+# de logged_in entre paginas
+st.session_state.count += 1
---
+++ pages/Inicia_sesion.py
@@ -0,0 +1,259 @@
+"""
+Módulo con todo lo necesario para el inicio de sesión de un usuario
+además de manejar el cierre de sesión
+"""
+
+# importar las liberías requeridas
+import streamlit as st 
+# pip install gspread
+import gspread 
+# pip install oauth2cliente
+from streamlit import session_state
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Título de la aplicación
+st.title("Login")
+
+# Formulario de login
+correo = st.text_input("Correo")
+contrasena = st.text_input("Contraseña", type="password")
+
+def obtener_nombre_usuario(user_correo):
+    """
+    Obtiene el nombre de usuario correspondiente a un correo específico desde una hoja de Google Sheets.
+
+    Parámetros:
+    - user_correo (str): El correo del usuario cuyo nombre se desea obtener.
+
+    Retorna:
+    - str: El nombre del usuario si se encuentra, o None si el correo no se encuentra en la hoja.
+    """
+
+    # Configuración de autenticación y acceso a Google Sheets
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive.file"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    cliente = gspread.authorize(creds)
+    hoja = cliente.open("Usuarios_bd").sheet1
+
+    # Obtiene todos los datos de la hoja
+    data = hoja.get_all_values()
+
+    # Busca el nombre del usuario correspondiente al correo
+    for row in data[1:]:  # Saltar la primera fila que son encabezados
+        if row[5] == user_correo:  # Suponiendo que el correo está en la sexta columna
+            return row[0]  # Suponiendo que el nombre está en la primera columna
+
+    return None  # Si no se encuentra el correo en la hoja
+
+def obtener_apellido_usuario(user_correo):
+    """
+    Obtiene el apellido de usuario correspondiente a un correo 
+    específico desde una hoja de Google Sheets.
+
+    Parámetros:
+    - user_correo (str): El correo del usuario cuyo nombre se desea obtener.
+
+    Retorna:
+    - str: El apellido del usuario si se encuentra, o None si el correo no se 
+    encuentra en la hoja.
+    """
+
+    # Configuración de autenticación y acceso a Google Sheets
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive.file"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    cliente = gspread.authorize(creds)
+    hoja = cliente.open("Usuarios_bd").sheet1
+
+    # Obtiene todos los datos de la hoja
+    data = hoja.get_all_values()
+
+    # Busca el nombre del usuario correspondiente al correo
+    for row in data[1:]:  # Saltar la primera fila que son encabezados
+        if row[5] == user_correo:  # Suponiendo que el correo está en la sexta columna
+            return row[1]  # Suponiendo que el nombre está en la primera columna
+
+    return None  # Si no se encuentra el correo en la hoja
+
+def obtener_genero_usuario(user_correo):
+    """
+    Obtiene el genero favorito de usuario correspondiente a un correo 
+    específico desde una hoja de Google Sheets.
+
+    Parámetros:
+    - user_correo (str): El correo del usuario cuyo nombre se desea obtener.
+
+    Retorna:
+    - str: El genero favorito del usuario si se encuentra, o None si el correo
+      no se encuentra en la hoja.
+    """
+
+    # Configuración de autenticación y acceso a Google Sheets
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive.file"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    cliente = gspread.authorize(creds)
+    hoja = cliente.open("Usuarios_bd").sheet1
+
+    # Obtiene todos los datos de la hoja
+    data = hoja.get_all_values()
+
+    # Busca el nombre del usuario correspondiente al correo
+    for row in data[1:]:  # Saltar la primera fila que son encabezados
+        if row[5] == user_correo:  # Suponiendo que el correo está en la sexta columna
+            return row[4]  # Suponiendo que el nombre está en la primera columna
+
+    return None  # Si no se encuentra el correo en la hoja
+
+def obtener_fecha_usuario(user_correo):
+    """
+    Obtiene la fecha de nacimiento de usuario correspondiente a un correo 
+    específico desde una hoja de Google Sheets.
+
+    Parámetros:
+    - user_correo (str): El correo del usuario cuyo nombre se desea obtener.
+
+    Retorna:
+    - str: La fecha de nacimiento del usuario si se encuentra, o None si el 
+    correo no se encuentra en la hoja.
+    """
+
+    # Configuración de autenticación y acceso a Google Sheets
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive.file"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    cliente = gspread.authorize(creds)
+    hoja = cliente.open("Usuarios_bd").sheet1
+
+    # Obtiene todos los datos de la hoja
+    data = hoja.get_all_values()
+
+    # Busca el nombre del usuario correspondiente al correo
+    for row in data[1:]:  # Saltar la primera fila que son encabezados
+        if row[5] == user_correo:  # Suponiendo que el correo está en la sexta columna
+            return row[2]  # Suponiendo que el nombre está en la primera columna
+
+    return None  # Si no se encuentra el correo en la hoja
+
+def obtener_sexo_usuario(user_correo):
+    """
+    Obtiene el sexo del usuario correspondiente a un correo específico 
+    desde una hoja de Google Sheets.
+
+    Parámetros:
+    - user_correo (str): El correo del usuario cuyo nombre se desea obtener.
+
+    Retorna:
+    - str: El sexo del usuario si se encuentra, o None si el correo no se 
+    encuentra en la hoja.
+    """
+
+    # Configuración de autenticación y acceso a Google Sheets
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive.file"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    cliente = gspread.authorize(creds)
+    hoja = cliente.open("Usuarios_bd").sheet1
+
+    # Obtiene todos los datos de la hoja
+    data = hoja.get_all_values()
+
+    # Busca el nombre del usuario correspondiente al correo
+    for row in data[1:]:  # Saltar la primera fila que son encabezados
+        if row[5] == user_correo:  # Suponiendo que el correo está en la sexta columna
+            return row[3]  # Suponiendo que el nombre está en la primera columna
+
+    return None  # Si no se encuentra el correo en la hoja
+
+
+def validar_credenciales(user_correo, user_contrasena):
+    """
+    Valida las credenciales de un usuario en una hoja de Google Sheets.
+
+    Parámetros:
+    - user_correo (str): El correo del usuario.
+    - user_contrasena (str): La contraseña del usuario.
+
+    Retorna:
+    - bool: True si las credenciales son válidas, False en caso contrario.
+    """
+
+    # Configuración de autenticación y acceso a Google Sheets
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive.file"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    cliente = gspread.authorize(creds)
+    hoja = cliente.open("Usuarios_bd").sheet1
+
+    # Obtiene todos los datos de la hoja
+    data = hoja.get_all_values()
+
+    # Recorre los datos y verifica las credenciales
+    for row in data[1:]:  # Saltar la primera fila que son encabezados
+        if row[5] == user_correo and row[6] == user_contrasena:  # Suponiendo que el correo está en la sexta columna y la contraseña en la séptima
+            return True  # Credenciales válidas
+
+    return False  # Credenciales inválidas
+
+# Botón para iniciar sesión
+if st.button("Iniciar sesión"):
+   
+   # Se llama la función para validar las credenciales ingresadas
+    if validar_credenciales(correo, contrasena):
+        st.success("Inicio de sesión exitoso")
+        
+        # Se crean las variables de sesión de estado para el usuario
+        st.session_state['correo'] = correo
+        st.session_state['logged_in']  = True
+        st.session_state.nombre = obtener_nombre_usuario(correo)
+        st.session_state.apellido = obtener_apellido_usuario(correo)
+        st.session_state.generofav = obtener_genero_usuario(correo)
+        st.session_state.sexo = obtener_sexo_usuario(correo)
+        st.session_state.fecha = obtener_fecha_usuario(correo)
+        
+        # Se obtiene el nombre de usuario para saludarlo
+        if st.session_state['logged_in']:
+            nombre_usuario = obtener_nombre_usuario(correo)
+            if nombre_usuario:
+                st.write(f"Bienvenido, {nombre_usuario}")
+            else:
+                 st.warning("No se pudo obtener el nombre del usuario")
+    else:
+        st.error("Usuario o contraseña incorrectos")
+        st.session_state['logged_in'] = False
+
+ 
+
+
+# Se verifica si el usuario está logeado para que aparezca
+# el boton de deslogeo
+if st.session_state['logged_in'] == True:
+    
+    if st.button("Cerrar sesión"):
+        st.session_state['logged_in']  = False
+
